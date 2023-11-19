33b929a012eddf787a89ec1fbbdf67abae75c1a6
Annie <annie.gouchee@mail.mcgill.ca>
2023-11-18 22:44:27 -0500

    square gallery

--- app.py
+++ app.py
@@ -10,14 +10,16 @@ from keras.preprocessing import image
 import numpy as np
 from werkzeug.utils import secure_filename
 from musicgen import gen_music
+from PIL import Image
 
-
+TEMP_FOLDER = 'static/temp'
 UPLOAD_FOLDER = 'static/uploads'
 model = ResNet152V2(weights='imagenet')    # Load the pre-trained ResNet50 model
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'random string'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['TEMP_FOLDER']= TEMP_FOLDER
 
 def generate_keywords(image_path):
     # Load and preprocess the image
@@ -52,13 +54,35 @@ def upload_file():
         flash('No selected file')
         return redirect(request.url)
     if file:
-        filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-        file.save(filename)
+        temp_filename = os.path.join(app.config['TEMP_FOLDER'], file.filename)
+        file.save(temp_filename)
+        filename=os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        crop_image_to_square(temp_filename,filename )
         results = generate_keywords(filename)
         keywords_string = ', '.join(results)
         print(keywords_string)
-        gen_music(keywords_string)
+        //gen_music(keywords_string)
         return redirect(url_for('index'))
+    
+def crop_image_to_square(source_path, destination_path):
+    img = Image.open(source_path)
+    width, height = img.size
+
+    # Find the smaller dimension to crop the square
+    min_dim = min(width, height)
+
+    # Calculate coordinates to crop a square
+    left = (width - min_dim) / 2
+    top = (height - min_dim) / 2
+    right = (width + min_dim) / 2
+    bottom = (height + min_dim) / 2
+
+    # Crop the image to a square
+    cropped_img = img.crop((left, top, right, bottom))
+
+    # Save the cropped image back to the file
+    cropped_img.save(destination_path)
+
 
 
 if __name__ == '__main__':
